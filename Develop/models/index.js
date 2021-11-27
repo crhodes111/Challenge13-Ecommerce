6fb0d3e2e23bbf0b8381d9fc11f5c6b62e53ba30
Conner Rhodes <connerrhodes.25@gmail.com>
2021-11-27 11:32:29 -0500

    Challenge 13 Working

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+//Each one has to reference the foreign key
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  foreignKeyConstraint: true,
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  foreignKeyConstraint: true,
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id'
+  //as
+  // foreignkey
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
   Tag,
   ProductTag,
 };
+
